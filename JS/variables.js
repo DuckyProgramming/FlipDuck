bc88bb672eaf61fb502ac3133c3c696dd59bbdbe
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2023-02-22 16:45:50 -0500

    Level25

--- JS/variables.js
+++ JS/variables.js
@@ -15,7 +15,7 @@ types={
     ],
 }
 stage={scale:0,focus:{x:0,y:0,scale:0},scene:'level'}
-game={zone:21,edge:{x:0,y:0},tileSize:30,check:{x:0,y:0,zone:0,gravity:1},position:{x:0,y:0},edgePosition:{x:17,y:17},firstGen:0}
+game={zone:25,edge:{x:0,y:0},tileSize:30,check:{x:0,y:0,zone:0,gravity:1},position:{x:0,y:0},edgePosition:{x:17,y:17},firstGen:0}
 physics={gravity:1.2,resistance:0.1,friction:0}
 graphics={main:0,backgrounds:[],minor:[]}
 transition={trigger:false,anim:0,scene:stage.scene,mode:0,direction:0}
